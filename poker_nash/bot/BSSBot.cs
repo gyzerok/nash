d4b0ddf3f8a00b3bd8cbf061e597b94d71ebad97
gyzerok <gyzerok@gmail.com>
2013-12-10 21:19:32 +0700

    Activity added

---
+++ poker_nash/bot/BSSBot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using common;
+
+namespace bot
+{
+    public class BSSBot : IBot
+    {
+        private Dictionary<List<string>, List<int>> preflopTable;
+
+        public BSSBot()
+        {
+            var sr = new StreamReader("preflop_table");
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var actions = new List<int>();
+                string tmpLine;
+                for (int i = 0; i < 3; i++)
+                {
+                    tmpLine = sr.ReadLine();
+                    actions = tmpLine.Split(' ').Select(n => int.Parse(n)).ToList();
+                    this.preflopTable.Add(line.Split(' ').ToList(), actions);
+                }
+            }
+
+            sr.Close();
+        }
+
+        public Activity Process(State state)
+        {
+
+            return null;
+        }
+    }
+}
--- poker_nash/bot/IBot.cs
+++ poker_nash/bot/IBot.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using common;
 
 namespace bot
 {
     public interface IBot
     {
-        void Process();
+        Activity Process(State state);
     }
 }
